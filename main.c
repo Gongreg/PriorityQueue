ef73b378142190cd776d528f843f4a0e56227ca3
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2016-02-17 10:57:18 +0200

    main.c changes to show working program

--- main.c
+++ main.c
@@ -4,7 +4,13 @@ int main()
 {
     Queue* queue;
     initQueue(&queue);
+    isEmptyQueue(queue);
+    pushQueue(queue, 5, 1);
+    pushQueue(queue, 4, 2);
+    pushQueue(queue, 5, 1);
+    pushQueue(queue, 4, 4);
+    pushQueue(queue, 3, 3);
+    popQueue(queue);
     destroyQueue(&queue);
-
     return 0;
 }
