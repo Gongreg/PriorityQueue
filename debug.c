eb409b64d683ed2b3efc25581f0248b1f65e9890
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2016-02-17 10:42:53 +0200

    LFF log and gitignore fixes

--- debug.c
+++ debug.c
@@ -10,6 +10,7 @@ int main()
     printMessage(pushQueue(queue, 4, 2));
     printMessage(pushQueue(queue, 5, 1));
     printMessage(pushQueue(queue, 4, 4));
+    printMessage(pushQueue(queue, 3, 3));
     mapQueue(queue);
     printMessage(popQueue(queue));
     mapQueue(queue);
